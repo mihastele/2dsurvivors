351e25a073e521fb61183f1d5a47a8a61a6ad13b
Miha <stele1000@gmail.com>
2024-12-23 16:26:56 +0100

    Lerp

--- scenes/game_camera.gd
+++ scenes/game_camera.gd
@@ -9,8 +9,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	acquire_target()
+	global_position = global_position.lerp(target_position, 1-exp(-delta * 10)); # framerate independent smooth lerp camera movement
+		
+
+func acquire_target():
 	var player_nodes = get_tree().get_nodes_in_group("player")
 	if player_nodes.size() > 0:
 		var player = player_nodes[0] as Node2D
 		target_position = player.global_position
-		#global_position = player.global_position
