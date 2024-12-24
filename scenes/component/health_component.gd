be5eeba877d99e90e1441e9553dc1f2dff17f23a
Miha <stele1000@gmail.com>
2024-12-24 19:53:39 +0100

    Add refactoring

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -11,7 +11,9 @@ func _ready() -> void:
 
 func damage(damage_amount: float):
 	current_health = max(current_health - damage_amount, 0)
+	Callable(check_death).call_deferred()
+	
+func check_death():
 	if current_health == 0:
 		died.emit()
 		owner.queue_free()
-	
--- vial_drop_component.gd
+++ vial_drop_component.gd
@@ -1,13 +1,18 @@
 extends Node
 
-@export var vial_scene: PackedScene
+@export_range(0, 1) var drop_percent: float = .5
 @export var health_component: Node
+@export var vial_scene: PackedScene
 
 func _ready() -> void:
 	(health_component as HealthComponent).died.connect(on_died)
 	
 	
 func on_died():
+	
+	if randf() >= drop_percent:
+		return
+	
 	if vial_scene == null:
 		return
 	
